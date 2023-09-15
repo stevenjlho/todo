dc571e60391c152c56be35c5bc3965253b4ff066
Steven Ho <stevenjlho@gmail.com>
2023-09-15 15:17:30 +0800

    fix: fix ts errors

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,8 +10,8 @@ const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
     }),
     // ...add more providers here
   ],
